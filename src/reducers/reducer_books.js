22ac93dd8fb659a4f8f9b54738131593d4d7b2c9
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-08-02 19:26:58 +0200

    update - lesson 4: 39

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        {title: "Leviathan Wakes" },
+        {title: "Solaris" },
+        {title: "Hitchhiker's Guide to the Galaxy" },
+        {title: "Dune" }
+    ]
+}
+
+/* reducer is a pure function */
